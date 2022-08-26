affb158409a37fe33d837b2ce11fbe1a4c182558
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-08-26 14:22:43 -0600

    Add styling for Nav.js component.

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,14 +5,15 @@ const Nav = () => {
   const [searchPhrase, setSearchPhrase] = useState('')
   return (
     <nav className='nav'>
-      <h1>Spotlight Cinema</h1>
+      <h1 className='app-title'>Spotlight Cinema</h1>
       <input 
+        className='search-bar-input'
         type='text'
         placeholder='Search by title...'
         value={searchPhrase}
         onChange={event => setSearchPhrase(event.target.value)}
       />
-        <select>
+        <select className='dropdown'>
           <option value="genres">All Genres</option>
           <option value="action">Action</option>
           <option value="horror">Horror</option>

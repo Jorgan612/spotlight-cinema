f2605bef62efc6ad8e25208c3118c1d8c143a38c
Susanna Carey <83846677+susannaopal@users.noreply.github.com>
2022-08-26 15:07:53 -0400

    Create file structure for components and css styling, begin creating nav with state

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../CSS/App.css'
+import Nav from './Nav';
+// import {useState, useEffect} from React;
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+    </>
+  )
+}
+export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,26 @@
+import '../CSS/Nav.css'
+import {useState} from 'react';
+
+const Nav = () => {
+  const [searchPhrase, setSearchPhrase] = useState('')
+  return (
+    <nav className='nav'>
+      <h1>Spotlight Cinema</h1>
+      <input 
+        type='text'
+        placeholder='Search by title...'
+        value={searchPhrase}
+        onChange={event => setSearchPhrase(event.target.value)}
+      />
+        <select>
+          <option value="genres">All Genres</option>
+          <option value="action">Action</option>
+          <option value="horror">Horror</option>
+          <option value="romance">Romance</option>
+        </select>
+    </nav>
+  )
+}
+
+export default Nav;
+

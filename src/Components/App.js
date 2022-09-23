7d8e9d873464206b01eb9a23e51bbeeaedab7f1b
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-09-23 16:42:10 -0600

    Add API fetch call and useeffect to pull data from api.

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,9 +3,31 @@ import '../SCSS/App.css';
 import Nav from './Nav';
 import Banner from './Banner'
 import MoviesContainer from './MoviesContainer';
-import { useState, useEffect } from React;
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [movies, setMovies] = useState([])
+  const [error, setError] = useState('')
+
+
+  const getMovies = async (page) => {
+    const url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=2'
+    setError('')
+
+    try {
+      const response = await fetch(url)
+      const movies = await response.json()
+      setMovies(movies)
+    } catch(error) {
+      setError(error.message)
+    }
+  }
+  
+  useEffect(() => {
+    getMovies();
+    console.log('movies', movies)
+  }, []) 
+
   return (
     <>
       <Nav />

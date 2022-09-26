e208ff6f6af4952bf0de8a8053546e96b7ca0807
Susanna Carey <83846677+susannaopal@users.noreply.github.com>
2022-09-26 15:19:33 -0400

    Creating functionality for adding mvies with buttons to API call

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,10 +8,11 @@ import { useState, useEffect } from 'react';
 const App = () => {
   const [movies, setMovies] = useState([])
   const [error, setError] = useState('')
+  const [pageCount, setPageCount] = useState(1)
 
 
   const getMovies = async () => {
-    const url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&page=5'
+    const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&page=${pageCount}`
     setError('')
 
     try {
@@ -25,9 +26,19 @@ const App = () => {
   
   useEffect(() => {
     getMovies();
-    console.log('movies', movies)
   }, []) 
 
+  const previousChangePage = () => {
+     setPageCount(pageCount - 1)
+    console.log(" - page count", pageCount)
+     getMovies();
+  }
+
+  const nextChangePage = () => {
+    setPageCount(pageCount + 1)
+    console.log(" + page count", pageCount)
+     getMovies();
+  }
 
   return (
     <>
@@ -35,6 +46,10 @@ const App = () => {
       <Banner />
       <div className='divider-div'></div>
       <MoviesContainer movies={movies} />
+      <div className='buttons-div'>
+        {pageCount > 1 && <button onClick={previousChangePage}>Previous</button>}
+        <button onClick={nextChangePage}>Next</button>
+      </div>
     </>
   )
 }

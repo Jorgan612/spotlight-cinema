46f2ed4a2fd8744e28405d2ffc7b1b15b895574c
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-09-27 16:00:24 -0600

    Attempt to fix async issue with page change. Begin Banner functionality.

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,22 +11,33 @@ const App = () => {
   const [pageCount, setPageCount] = useState(1)
 
 
+  // for search functionality test this url: 
+  // https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+
+
   const getMovies = async () => {
     const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&page=${pageCount}`
     setError('')
 
     try {
-      const response = await fetch(url)
-      const movies = await response.json()
-      setMovies(movies.results)
+      const response = await fetch(url);
+      const movies = await response.json();
+      setMovies(movies.results);
     } catch(error) {
-      setError(error.message)
+      setError(error.message);
     }
   }
   
   useEffect(() => {
     getMovies();
+    // randomMovie();
   }, []) 
+
+  // const randomMovie = () => {
+  //   console.log(movies)
+  //   return Math.floor(Math.random() * movies.length)
+  //   // console.log('rando', rando)
+  // }
 
   const previousChangePage = () => {
      setPageCount(pageCount - 1)
@@ -43,12 +54,13 @@ const App = () => {
   return (
     <>
       <Nav />
-      <Banner />
+      <Banner movies={movies} />
       <div className='divider-div'></div>
       <MoviesContainer movies={movies} />
       <div className='buttons-div'>
         {pageCount > 1 && <button onClick={previousChangePage}>Previous</button>}
         <button onClick={nextChangePage}>Next</button>
+        <p>{pageCount}</p>
       </div>
     </>
   )
--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,8 +1,47 @@
-import '../SCSS/Banner.css'
+import { useEffect } from 'react';
+import '../SCSS/Banner.css';
+import MovieCard from './MovieCard';
 
-const Banner = () => {
+const Banner = ({movies}) => {
+  
+  
+  // left off looking at Suzie's example 
+
+
+
+  // return (
+
+  // )
+
+  let random = Math.floor(Math.random() * movies.length)
+console.log('randomMovie', random)
+console.log('Movies array', movies[random])
+  // console.log('______', movies)
+  // const randomMovie = () => {
+  //   let rando =  Math.floor(Math.random() * movies.length)
+  //   console.log('rando', rando)
+  //   return rando;
+  // }
+
+  render() {
+    const filteredQuoteCards = this.state.filteredQuotes.map((quote, index) => {
+      return (
+        <Card
+          text={quote.text}
+          author={quote.author}
+          id={index}
+          key={index}
+       />
+    )
+  });
+
   return (
     <section className='banner-section'>
+      <MovieCard />
+      <p>TEST</p>
+      <p>{random}</p>
+      <p>{random.title}</p>
+      {/* <MovieCard rando={randomMovie}/>  */}
     </section>
   )
 }

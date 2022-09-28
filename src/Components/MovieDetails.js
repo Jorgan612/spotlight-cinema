6f5146cade755c3b7b2a797d168416a8551a782c
Susanna Carey <83846677+susannaopal@users.noreply.github.com>
2022-09-28 15:15:55 -0400

    Create single view with details

--- src/Components/MovieDetails.js
+++ src/Components/MovieDetails.js
@@ -1,6 +1,12 @@
 const MovieDetails = ({singleView}) => {
   return (
     <div className='movie-details-div'>
+      <h1>{singleView.title}</h1>
+      <h2>{singleView.overview}</h2>
+      <p>{Number(singleView.popularity.toFixed())}</p>
+      <p>{singleView.release_date}</p>
+      <p>{Number(singleView.vote_average.toFixed(1))}</p>
+      <p>{singleView.vote_count}</p>
       <img className='backdrop-img' id={singleView.id} src={`https://image.tmdb.org/t/p/w500${singleView.backdrop_path}`} alt='single view poster'/>
     </div>
   )

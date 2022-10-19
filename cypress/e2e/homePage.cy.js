7be035356f3929b0c7afa60995e354c91d63d801
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-10-19 17:20:27 -0600

    Continue troubleshooting intercepts for API testing with Cypress.

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -1,11 +1,40 @@
 describe('Home Page User Flow', () => {
     beforeEach(() => {
-      cy.intercept('GET', 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&page=500', {fixture: "movies.json"})
-        // .visit('http://localhost:3000/');
+      // cy.intercept('GET', 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&page=500', {
+      //   statusCode: 201,
+      //   body: {
+      //     id: 2,
+      //     image: "https://ca.slack-edge.com/T029P2S9M-U37MJAV0T-007ccf2f5eb2-512",
+      //     name: "Leta Keane"
+      //   }
+      // })
 
-        // https://api.themoviedb.org/3/movie/${randomNum}?api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&language=en-US&append_to_response=videos
-      // .intercept('GET', 'https://api.themoviedb.org/3/movie/900?api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&language=en-US&append_to_response=videos', {fixture: "video.json"})
-      .visit('http://localhost:3000/');
+      // cy.intercept('GET', 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&page=500', (req) => {
+      //   req.reply({
+      //     statusCode: 200, // default
+      //     fixture: 'movies.json'
+      //   })
+      // })
+
+      cy.intercept('GET', 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&page=500').as('movies.json')
+        .visit('http://localhost:3000/')
+
+        // cy.intercept(
+        //   {
+        //     method: 'GET',
+        //     path: 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&page=500',
+        //   },
+        //   {
+        //     fixture: 'movies.json',
+        //   },
+        //   ).as('movies.json')
+        //    .visit('http://localhost:3000/')
+
+      // cy.intercept('GET', 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&page=500', {fixture: 'movies.json'})
+      //   .visit('http://localhost:3000/');
+
+      // cy.intercept('GET', 'https://api.themoviedb.org/3/movie/900?api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&language=en-US&append_to_response=videos', {fixture: "video.json"})
+      // .visit('http://localhost:3000/');
     });
     
     it('should be able to visit the url', () => {
@@ -27,6 +56,14 @@ describe('Home Page User Flow', () => {
 
     it('Should see a movie banner and trailer below navbar', () => {
       cy.get('.banner-section')
+        .should('be.visible')
+    })
+
+    it('Should see a list of movie posters after the video banner', () => {
+      cy.intercept('GET', 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eb5e7e86d8d7c0c5c8fe773faa42a22e&page=500', {fixture: 'movies.json'})
+        .visit('http://localhost:3000/');
+
+      cy.get('.movies-container')
         .should('be.visible')
     })
   })

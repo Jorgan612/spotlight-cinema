3233a7ab23d69047e8bddcf1b052333c0a02230b
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-10-20 12:19:37 -0600

    Add button to poster to add to watchlist, check functionality of button. Continue working on watchlist functionality next session.

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -3,12 +3,16 @@ import '../SCSS/MovieCard.css'
 import MovieDetails from './MovieDetails';
 
 const MovieCard  = ( {img, id, getSingleMovieDetails} ) => {
+  const addToWatchList = () => {
+    console.log('can you click watchlist button?')
+  }
   //click should take viewer to a single movie's details
   //need a router link to it
   //brings you to that page, activates that component, so that component is rendered
   return (
     <div className='movie-card banner-card' onClick={() => getSingleMovieDetails(id)}>
       <Link to='/moviedetails'><img className='poster-img banner-img' id={id} src={`https://image.tmdb.org/t/p/w500${img}`} alt='movie poster'/></Link>
+      <button className='watchlist-add btn' onClick={addToWatchList}>➕</button>
     </div>
   )
 }

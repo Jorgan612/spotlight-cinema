a9cabc4ddb99b1eccaedc10f154bcc34f1519caa
Susanna Carey <83846677+susannaopal@users.noreply.github.com>
2022-11-01 14:35:54 -0400

    Update to add a back button to return home

--- src/Components/WatchList.js
+++ src/Components/WatchList.js
@@ -1,18 +1,23 @@
 import MovieCard from "./MovieCard";
+import '../SCSS/WatchList.css';
+import { Link } from 'react-router-dom';
 
-const WatchList = ({watchList, movies, getSingleMovieDetails}) => {
+const WatchList = ({watchList, getSingleMovieDetails}) => {
   const watchListTitles = watchList.map((movie, index) => {
      return <MovieCard 
-      img={movie.poster_path}
-      key={index}
-      id={movie.id}
-      getSingleMovieDetails={getSingleMovieDetails}
-    />
+        img={movie.poster_path}
+        key={index}
+        id={movie.id}
+        getSingleMovieDetails={getSingleMovieDetails}
+      />
   })
   return (
-    <div className='watchlist-titles'>
+    <div className='watchlist-titles-div'>
       <h1>Watchlist</h1>
       {watchListTitles}
+      <div>
+        <Link to='/'><button>Back</button></Link>
+      </div>
     </div>
   )
 }
